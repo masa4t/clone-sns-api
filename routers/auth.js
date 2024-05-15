d86d3311ebce5ff7b85834e5420360de595275ed
masa4t <godo034t@gmail.com>
2024-05-15 20:15:01 +0900

    final commit

--- routers/auth.js
+++ routers/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const prisma = new PrismaClient();
