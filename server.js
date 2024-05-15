80692eea0c9c5dc5c63faf15cd2d05a44d050d43
masa4t <godo034t@gmail.com>
2024-05-15 19:18:20 +0900

    maybe last push

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const profileRoutes = require("./routers/profile");
 require("dotenv").config();
 const cors = require("cors");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
