9fd16e4e616cdfed5aef3fb5362725ae164a23d4
masa4t <godo034t@gmail.com>
2024-05-16 00:32:38 +0900

    final commit

--- routers/profile.js
+++ routers/profile.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const multer = require("multer");
 const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
 const prisma = new PrismaClient();
 const router = express.Router();
